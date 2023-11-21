bb2ad2ba221eda1e59b69a7581f68ba26f5da4b9
Ivan Danyleiko <147879140+Ivan-Danyleiko@users.noreply.github.com>
2023-11-21 15:42:19 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,7 +1,6 @@
 import os
 import sys
-import zipfile
-from pathlib import Path
+import shutil
 
 def normalize(input_str):
     translit_mapping = {
@@ -11,19 +10,17 @@ def normalize(input_str):
         'ш': 'sh', 'щ': 'shch', 'ь': '', 'ю': 'iu', 'я': 'ia'
     }
 
-    normalized_str = ''
-    for char in input_str:
+    name, extension = os.path.splitext(input_str)
+    normalized_name = ''
+    for char in name:
         if char.lower() in translit_mapping:
-            normalized_str += translit_mapping[char.lower()]
+            normalized_name += translit_mapping[char.lower()]
         elif char.isalnum():
-            normalized_str += char.lower()
+            normalized_name += char.lower()
         else:
-            normalized_str += '_'
+            normalized_name += '_'
 
-    return normalized_str
-
-def get_archive_name_without_extension(file_path):
-    return Path(file_path).stem
+    return f"{normalized_name}{extension}"
 
 def categorize_file(file_path):
     extension = file_path.split('.')[-1].upper()
@@ -40,35 +37,8 @@ def categorize_file(file_path):
     else:
         return 'unknown'
 
-def categorize_and_move_file(file_path, destination_folder):
-    category = categorize_file(file_path)
-    print(f"Категоризація файлу: {file_path}, Категорія: {category}")
-
-    sorted_folder_path = os.path.join(os.path.abspath(destination_folder), category)
-    print(f"Шлях сортованої папки: {sorted_folder_path}")
-
-    # Створіть папку, якщо вона не існує
-    Path(sorted_folder_path).mkdir(parents=True, exist_ok=True)
-
-    if category == 'archives':
-        archive_name_without_extension = get_archive_name_without_extension(file_path)
-        new_extraction_path = os.path.join(sorted_folder_path, normalize(archive_name_without_extension))
-        with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            zip_ref.extractall(sorted_folder_path)
-        os.rename(os.path.join(sorted_folder_path, archive_name_without_extension), new_extraction_path)
-        return new_extraction_path
-    else:
-        normalized_name = normalize(os.path.basename(file_path))
-        new_file_path = os.path.join(sorted_folder_path, normalized_name)
-        os.rename(file_path, new_file_path)
-        return new_file_path
-
-def process_folder(folder_path):
+def process_folder(folder_path, destination_folder, empty_folders):
     print(f"Обробка папки: {folder_path}")
-
-    # Створити папку 'sorted' в вказаній директорії
-    sorted_folder_path = os.path.join(folder_path, 'sorted')
-    Path(sorted_folder_path).mkdir(parents=True, exist_ok=True)
 
     items = os.listdir(folder_path)
 
@@ -76,11 +46,50 @@ def process_folder(folder_path):
         item_path = os.path.join(folder_path, item)
 
         if os.path.isfile(item_path):
-            categorize_and_move_file(item_path, sorted_folder_path)
+            destination = categorize_file(item_path)
+            normalized_name = normalize(os.path.basename(item_path))
+
+            if destination == 'unknown':
+                extension = os.path.splitext(item)[1].lstrip('.').lower()
+                normalized_name += f'.{extension}'
+
+            new_file_path = os.path.join(destination_folder, destination, normalized_name)
+            os.makedirs(os.path.dirname(new_file_path), exist_ok=True)
+            shutil.move(item_path, new_file_path)
+
+        elif os.path.isdir(item_path):
+            process_folder(item_path, destination_folder, empty_folders)
+
+    # Перевірте, чи папка порожня після обробки файлів
+    if not os.listdir(folder_path):
+        empty_folders.append(folder_path)
+
+def create_category_folders(root_folder, destination_folder):
+    categories = ['images', 'video', 'documents', 'audio', 'archives', 'unknown']
+    for category in categories:
+        if category not in ['archives', 'video', 'audio', 'documents', 'images']:
+            category_path = os.path.join(destination_folder, category)
+            os.makedirs(category_path, exist_ok=True)
+
+def remove_empty_folders(folder):
+    for root, dirs, files in os.walk(folder, topdown=False):
+        for dir in dirs:
+            dir_path = os.path.join(root, dir)
+            if not os.listdir(dir_path):
+                try:
+                    os.rmdir(dir_path)
+                    print(f"Порожню папку видалено: {dir_path}")
+                except OSError as e:
+                    print(f"Не вдалося видалити порожню папку {dir_path}: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Використання: python DZ.py <шлях_папки>")
+        print("Використання: python <шлях_скрипта> <шлях_папки>")
     else:
         folder_path = sys.argv[1]
-        process_folder(folder_path)
+        destination_folder = folder_path  # Папка призначення - це головна директорія
+        create_category_folders(folder_path, destination_folder)
+        empty_folders = []  # Список порожніх папок для видалення
+        process_folder(folder_path, destination_folder, empty_folders)
+        remove_empty_folders(folder_path)
+        remove_empty_folders(destination_folder)
